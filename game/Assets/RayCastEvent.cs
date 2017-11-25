2a21284bb4348d8de89f29750ee41e7621d095d3
dBanasiak <damian.banasiak96@gmail.com>
2017-11-25 14:00:23 +0100

    Konrad pisze połowę gry

---
+++ game/Assets/RayCastEvent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RayCastEvent : MonoBehaviour {
+
+	public Camera cam;
+	Ray ray;
+	RaycastHit hit;
+	GameObject lookingAt;
+	int uiLayer = 1 << 5;
+	float lookingTimer = 0;
+	public float maxTime;
+	// Use this for initialization
+	void Start () {
+		Debug.Log(LayerMask.NameToLayer("UI"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+		if (Physics.Raycast (ray, out hit, 100, uiLayer)) {
+			if (lookingAt == hit.collider.gameObject) {
+				lookingTimer += Time.deltaTime;
+				print (lookingTimer);
+				if (lookingTimer > maxTime) {
+					switch (lookingAt.name) {
+					case "Start":
+						print ("Start");
+						SceneManager.LoadScene("gra", LoadSceneMode.Single);
+						break;
+					case "Opcje":
+						print ("Opcje");
+						break;
+					case "Wyjście":
+						print ("Wyjście");
+						Application.Quit ();
+						break;
+					}
+				}
+			} else {
+				lookingTimer = 0;
+				lookingAt = hit.collider.gameObject;
+			}
+			print ("I'm looking at " + hit.transform.name);
+		} else {
+			lookingAt = null;
+			print ("I'm looking at nothing!");
+		}
+	}
+}
---
+++ game/Assets/Swimming.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Swimming : MonoBehaviour {
+	public float speed;
+	public Transform ts;
+	public GameObject go;
+	public float maxTlen;
+	float currentTlen;
+	public float reklamowka;
+	public Image red;
+	// Use this for initialization
+	void Start () {
+		currentTlen = maxTlen;
+	}
+
+	void Update() {
+		red.color = new Color (1, 0, 0, (maxTlen - currentTlen) / maxTlen);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		currentTlen -= Time.deltaTime;
+		if (currentTlen <= 0) {
+			SceneManager.LoadScene ("gra");
+		}
+		transform.Translate(ts.forward * speed * Time.deltaTime);
+	}
+	public void buttonClick()
+	{
+		go.GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	void OnTriggerEnter(Collider col) {
+
+		if (col.CompareTag("Wall")) {
+			SceneManager.LoadScene ("gra");
+		}
+		if (col.CompareTag ("Siatka")) {
+			currentTlen += reklamowka;
+			Destroy (col.gameObject);
+		}
+	}
+
+}
